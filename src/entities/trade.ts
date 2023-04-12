71ce88ab872efbbf9cdb04ed914153a9febe0ebe
oceanswapdefi <130521778+oceanswapdefi@users.noreply.github.com>
2023-04-11 22:21:41 -0400

    Update trade.ts

--- src/entities/trade.ts
+++ src/entities/trade.ts
@@ -88,13 +88,13 @@ export interface BestTradeOptions {
  * In other words, if the currency is ETHER, returns the WETH token amount for the given chain. Otherwise, returns
  * the input currency amount.
  */
-function wrappedAmount(currencyAmount: CurrencyAmount, chainId: ChainId = ChainId.AVALANCHE): TokenAmount {
+function wrappedAmount(currencyAmount: CurrencyAmount, chainId: ChainId = ChainId.PULSE_TESTNET): TokenAmount {
   if (currencyAmount instanceof TokenAmount) return currencyAmount
   if (currencyAmount.currency === CAVAX[chainId]) return new TokenAmount(WAVAX[chainId], currencyAmount.raw)
   invariant(false, 'CURRENCY')
 }
 
-function wrappedCurrency(currency: Currency, chainId: ChainId = ChainId.AVALANCHE): Token {
+function wrappedCurrency(currency: Currency, chainId: ChainId = ChainId.PULSE_TESTNET): Token {
   if (currency instanceof Token) return currency
   if (currency === CAVAX[chainId]) return WAVAX[chainId]
   invariant(false, 'CURRENCY')
@@ -134,14 +134,14 @@ export class Trade {
    */
   public readonly priceImpact: Percent
 
-  public readonly chainId: ChainId = ChainId.AVALANCHE
+  public readonly chainId: ChainId = ChainId.PULSE_TESTNET
 
   /**
    * Constructs an exact in trade with the given amount in and route
    * @param route route of the exact in trade
    * @param amountIn the amount being passed in
    */
-  public static exactIn(route: Route, amountIn: CurrencyAmount, chainId: ChainId = ChainId.AVALANCHE): Trade {
+  public static exactIn(route: Route, amountIn: CurrencyAmount, chainId: ChainId = ChainId.PULSE_TESTNET): Trade {
     return new Trade(route, amountIn, TradeType.EXACT_INPUT, chainId)
   }
 
@@ -150,11 +150,11 @@ export class Trade {
    * @param route route of the exact out trade
    * @param amountOut the amount returned by the trade
    */
-  public static exactOut(route: Route, amountOut: CurrencyAmount, chainId: ChainId = ChainId.AVALANCHE): Trade {
+  public static exactOut(route: Route, amountOut: CurrencyAmount, chainId: ChainId = ChainId.PULSE_TESTNET): Trade {
     return new Trade(route, amountOut, TradeType.EXACT_OUTPUT, chainId)
   }
 
-  public constructor(route: Route, amount: CurrencyAmount, tradeType: TradeType, chainId: ChainId = ChainId.AVALANCHE) {
+  public constructor(route: Route, amount: CurrencyAmount, tradeType: TradeType, chainId: ChainId = ChainId.PULSE_TESTNET) {
     const amounts: TokenAmount[] = new Array(route.path.length)
     const nextPairs: Pair[] = new Array(route.pairs.length)
     if (tradeType === TradeType.EXACT_INPUT) {
@@ -206,7 +206,7 @@ export class Trade {
    * Get the minimum amount that must be received from this trade for the given slippage tolerance
    * @param slippageTolerance tolerance of unfavorable slippage from the execution price of this trade
    */
-  public minimumAmountOut(slippageTolerance: Percent, chainId: ChainId = ChainId.AVALANCHE): CurrencyAmount {
+  public minimumAmountOut(slippageTolerance: Percent, chainId: ChainId = ChainId.PULSE_TESTNET): CurrencyAmount {
     invariant(!slippageTolerance.lessThan(ZERO), 'SLIPPAGE_TOLERANCE')
     if (this.tradeType === TradeType.EXACT_OUTPUT) {
       return this.outputAmount
@@ -225,7 +225,7 @@ export class Trade {
    * Get the maximum amount in that can be spent via this trade for the given slippage tolerance
    * @param slippageTolerance tolerance of unfavorable slippage from the execution price of this trade
    */
-  public maximumAmountIn(slippageTolerance: Percent, chainId: ChainId = ChainId.AVALANCHE): CurrencyAmount {
+  public maximumAmountIn(slippageTolerance: Percent, chainId: ChainId = ChainId.PULSE_TESTNET): CurrencyAmount {
     invariant(!slippageTolerance.lessThan(ZERO), 'SLIPPAGE_TOLERANCE')
     if (this.tradeType === TradeType.EXACT_INPUT) {
       return this.inputAmount
